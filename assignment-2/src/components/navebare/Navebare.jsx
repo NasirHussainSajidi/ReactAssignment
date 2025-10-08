dfc86ead54a43854f1410a9f1593f3cebdfb7e35
NasirHussainSajidi <nasirsajidi313@gmail.com>
2025-10-08 23:46:31 +0500

    assignment-2

---
+++ assignment-2/src/components/navebare/Navebare.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const navebare = () =>{
+    return (
+        <div className="flex">
+            <h1>LOGO</h1>
+            <div>
+                <li href="#">Home</li>
+                <li href="#">About</li>
+                <li href="#">Blog</li>
+                <li href="#">Contact</li>
+            </div>
+        </div>
+    )
+}
